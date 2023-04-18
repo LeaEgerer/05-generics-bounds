751984b9e723b869078f1bb7bab85f500c868300
Lea Egerer <egererle70463@th-nuernberg.de>
2023-04-18 11:34:35 +0200

    created class Flower

---
+++ src/main/java/ohm/softa/a05/collections/Comparable.java
@@ -0,0 +1,5 @@
+package ohm.softa.a05.collections;
+
+public interface Comparable<T> {
+    public int compareTo(T o);
+}
---
+++ src/main/java/ohm/softa/a05/collections/Plant.java
@@ -0,0 +1,19 @@
+package ohm.softa.a05.collections;
+
+public abstract class Plant {
+    public double height;
+    public String family;
+    public String name;
+
+    public double getHeight(){
+        return height;
+    }
+
+    public String  getFamily(){
+        return family;
+    }
+
+    public String getName(){
+        return name;
+    }
+}
